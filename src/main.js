8529cb8245e7e9d143095f02f999b749387d71a2
Elena Ordina <post032@yandex.ru>
2021-01-15 18:21:41 +0300

    oop start

--- src/main.js
+++ src/main.js
@@ -1,15 +1,12 @@
+import SiteMenuView from './view/menu';
 import {createRouteInfoTemplate} from './view/route-info';
 import {createPriceTemplate} from './view/price';
 import {createFilterTemplate} from './view/filter';
-import {createMenuTemplate} from './view/menu';
 import {createSortTemplate} from './view/sort';
 import {createEditPointTemplate} from './view/edit-point';
 import {createPointTemplate} from './view/point';
 import {points} from "./mock/data";
-const render = (container, template, place) => {
-  container.insertAdjacentHTML(place, template);
-};
-
+import {renderTemplate, renderElement, RenderPosition} from "./mock/utils";
 
 const headerElement = document.querySelector(`header`);
 const tripInfo = headerElement.querySelector(`.trip-info`);
@@ -18,12 +15,12 @@ const tripControl = headerElement.querySelector(`.trip-controls`);
 const tripEvents = mainElement.querySelector(`.trip-events h2`);
 const tripEventsLists = mainElement.querySelector(`.trip-events__list`);
 
-render(tripInfo, createPriceTemplate(),`afterBegin`);
-render(tripInfo, createRouteInfoTemplate(), `afterBegin`);
-render(tripControl, createFilterTemplate(), `afterBegin`);
-render(tripControl, createMenuTemplate(), `afterBegin`);
-render(tripEvents, createSortTemplate(), `afterEnd`);
-render(tripEventsLists, createEditPointTemplate(points[0]), `afterBegin`);
+renderTemplate(tripInfo, createPriceTemplate(), `afterBegin`);
+renderTemplate(tripInfo, createRouteInfoTemplate(), `afterBegin`);
+renderTemplate(tripControl, createFilterTemplate(), `afterBegin`);
+renderElement(tripControl, new SiteMenuView().getElement(), RenderPosition.AFTERBEGIN);
+renderTemplate(tripEvents, createSortTemplate(), `afterEnd`);
+renderTemplate(tripEventsLists, createEditPointTemplate(points[0]), `afterBegin`);
 points.forEach((point) => {
-  render(tripEventsLists, createPointTemplate(point), `beforeEnd`);
+  renderTemplate(tripEventsLists, createPointTemplate(point), `beforeEnd`);
 });
--- src/view/menu.js
+++ src/view/menu.js
@@ -1,6 +1,28 @@
-export const createMenuTemplate = () =>
-  `<h2 class="visually-hidden">Switch trip view</h2>
+import {createElement} from '../mock/utils';
+
+const createMenuTemplate = () => {
+  return `<h2 class="visually-hidden">Switch trip view</h2>
     <nav class="trip-controls__trip-tabs  trip-tabs">
       <a class="trip-tabs__btn  trip-tabs__btn--active" href="#">Table</a>
       <a class="trip-tabs__btn" href="#">Stats</a>
     </nav>`;
+};
+
+export default class SiteMenu {
+  constructor() {
+    this._element = null;
+  }
+  getTemplate() {
+    return createMenuTemplate();
+  }
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
